fa74534c7bae0813407b5aeb88d8d35d588132d3
Erik Assum <erik@iterate.no>
2015-10-10 21:50:46 +0200

    Adds basic calendar And I said basic

--- src/hours/handler.clj
+++ src/hours/handler.clj
@@ -17,13 +17,35 @@
 (defn add-hours [m]
   (swap! hours conj m))
 
-
 (def custom-formatter (f/formatter "dd/MM-yy"))
 
+(defn prev-monday [dt]
+  (let [mon 1
+        today (t/day-of-week dt)]
+    (if (= mon today)
+      dt
+      (t/minus dt (t/days (dec today)))))) 
+
+(defn prev-week [mon]
+  (t/minus mon (t/weeks 1)))
+
+(defn next-week [mon]
+  (t/plus mon (t/weeks 1)))
+
+(defn add-days [dt i]
+  (t/plus dt (t/days i)))
+
+(defn week [dt]
+  (map (partial add-days (prev-monday dt)) (range 0 7)))
 
 (defn ->hour-mins [interval]
   (let [minutes (t/in-minutes interval)]
     (vector (int (/ minutes 60)) (rem minutes 60))))
+
+(defn trunc-seconds [dt]
+  (-> dt
+      (t/minus (t/seconds (t/second dt)))
+      (t/minus (t/millis (t/milli dt)))))
 
 (defn format-interval [hours-mins]
   (apply format (cons "%02d:%02d" hours-mins)))
@@ -34,6 +56,27 @@
 (defn by-date [hours]
   (let [days (group-by (fn [period] (f/unparse custom-formatter (:start period))) hours)]
     (mapcat find-total-by-day days)))
+
+(defn display-week-chooser [mon]
+  (let [prev (prev-week mon)
+        next (next-week mon)]
+    [:div "Uke " [:a {:href (str "/week/" (f/unparse (f/formatters :basic-date) prev )) } (.getWeekOfWeekyear prev) ]
+     " "
+     (.getWeekOfWeekyear mon)
+     " "
+     [:a {:href (str "/week/" (f/unparse (f/formatters :basic-date) next )) } (.getWeekOfWeekyear next) ]
+     ]))
+
+(defn display-week [week]
+  [:div
+   (display-week-chooser (first week))
+   [:table
+   [:tbody
+    [:tr
+     [:th "Date"]]
+    (for [day week]
+      [:tr
+       [:td (f/unparse custom-formatter day)]])]]])
 
 (defn display-hours [hours]
   [:table
@@ -55,39 +98,40 @@
           [:td ]])
       )]])
 
-(defn page-template [action content]
+(defn start-stop [action content]
+  [:div [:form {:method "POST" :action (str "/register/" action) }
+   (anti-forgery-field)
+   [:tr
+    [:td.submit {:colspan 2}
+     [:input {:type "submit" :value action}]]]]
+   content])
+
+
+(defn page-template [content]
   (html5
    [:head
     [:title "hours"]]
    [:body
     [:h1 [:a {:href "/"} "hours"]]
-    [:form {:method "POST" :action (str "/register/" action) }
-     (anti-forgery-field)
-     [:tr
-      [:td.submit {:colspan 2}
-       [:input {:type "submit" :value action}]]]]
-    content]))
-
-(defn trunc-seconds [dt]
-  (-> dt
-      (t/minus (t/seconds (t/second dt)))
-      (t/minus (t/millis (t/milli dt)))))
+    [:div.content content]]))
 
 (defn start []
   (swap! current assoc :start (trunc-seconds (t/now)))
-  (page-template "stop" (display-hours @hours)))
+  (page-template (start-stop "stop" (display-hours @hours))))
 
 (defn stop []
   (swap! current assoc :stop (trunc-seconds (t/now)))
   (add-hours @current)
   (reset! current {})
-  (page-template "start" (display-hours @hours)))
+  (page-template  (start-stop "start" (display-hours @hours)) ))
 
 (defroutes app-routes
-  (GET "/" [] (page-template "start" ""))
+  (GET "/" [] (page-template  (start-stop "start" (display-hours @hours))))
+  (GET "/week" [] (page-template (display-week (week (t/now)))))
+  (GET "/week/:date" [date] (page-template (display-week (week (f/parse (f/formatters :basic-date) date)))))
   (POST "/register/start" [] (start))
   (POST "/register/stop" [] (stop))  
-  (route/not-found "Not Found"))
+  (route/not-found "not found"))
 
 (def app
   (wrap-defaults #'app-routes site-defaults))
