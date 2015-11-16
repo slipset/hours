cc14825f7ea64c7f1b95d846cfb12179db709e95
Erik Assum <erik@iterate.no>
2015-11-16 16:58:30 +0100

    not used anymore

--- test/hours/reports/report_test.clj
+++ test/hours/reports/report_test.clj
@@ -2,9 +2,5 @@
     (:require [hours.reports.report :as sut]
               [clojure.test :refer :all]))
 
-(deftest test-grand-total
-  (is (= 8 (sut/grand-total {:foo '({:total 4} {:total 1})
-                             :bar '({:total 3})}))))
-
 (deftest test-week-total
   (is (= 8 (sut/week-total [{:total 1}{:total 1} {:total 1} {:total 1} {:total 1} {:total 1} {:total 2}]))))
